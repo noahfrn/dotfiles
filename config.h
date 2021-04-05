41bdd9099adc219b388565893adf583f73d52e5f
NoahFournier <fourniernoah6@gmail.com>
2021-04-05 15:06:41 +0100

    update dwm config

--- config.h
+++ config.h
@@ -8,8 +8,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "mononoki Nerd Font:size=11" };
+static const char dmenufont[]       = "mononoki Nerd Font:size=11";
 
 /*
  * Base16 dwm template by Daniel Mulford
