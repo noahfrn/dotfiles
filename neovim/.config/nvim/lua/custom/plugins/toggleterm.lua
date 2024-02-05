749564f93db3701ff3f3647f77f5621bc358aed2
noahfrn <noah@solveintelligence.com>
2024-02-05 11:37:52 +0000

    Add lazygit

--- neovim/.config/nvim/lua/custom/plugins/toggleterm.lua
+++ neovim/.config/nvim/lua/custom/plugins/toggleterm.lua
@@ -1,5 +1,5 @@
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true, silent = true}
+  local opts = { noremap = true, silent = true }
   vim.api.nvim_buf_set_keymap(0, 't', '<Esc>', '<C-\\><C-n>', opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', '<C-\\><C-n><C-w>h', opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', '<C-\\><C-n><C-w>j', opts)
@@ -29,13 +29,24 @@ return {
         border = 'curved',
         winblend = 0,
       },
-      highlights = {
-        border = "Normal",
-        background = "Normal",
-      }
     })
 
 
     vim.cmd('autocmd TermOpen term://* lua set_terminal_keymaps()')
+
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      hidden = true,
+      direction = "tab",
+    })
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+    require("which-key").register({
+      ["<leader>gg"] = { "Lazygit" }
+    })
   end
 }
